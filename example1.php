e7770f78f893f4ed63fb01a6a695aa6c68ebff05
joyelmk <joyelmkej3@gmail.com>
2022-12-14 11:02:05 +0530

    new text added

---
+++ example1.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <style>
+        h1 {
+            color: green;
+        }
+    </style>
+</head>
+  
+<body>
+    <center>
+        <h1>Welcome To GFG</h1>
+  
+        <h2>
+            <?php 
+            echo "This is PHP code inside html<br>";
+            echo "new text added"
+            ?>
+        </h2>
+    </center>
+</body>
+  
+</html>
