a067571a420d80b2b8ea4b7233ef0c5061b91789
joyelmk <joyelmkej3@gmail.com>
2022-12-14 12:27:56 +0530

    names added

---
+++ .history/names_20221214121739.php
@@ -0,0 +1,26 @@
+<?php
+$names=['james','elsy', 'jesly'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="">
+    <h1>NAMES</h1>
+    <ul>
+        <?php foreach($names as $names){?>
+            <li><?=$names</li>
+            <
+
+        }
+    </ul>
+
+
+</body>
+</html>
